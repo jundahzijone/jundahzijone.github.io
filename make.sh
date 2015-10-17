3c2ca0d74f357894e14ded9b87e357f13b94b5eb
hzijone <huangzhigang@junda-tech.com>
2015-10-17 15:24:24 +0800

    add some layout to asciidoc file and modify the _config.yml to support the adoc file

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "hello"
 git add -A
-git commit -m "add some layout to asciidoc file and modify the _config.yml to support the adoc file "
+git commit -am "add some layout to asciidoc file and modify the _config.yml to support the adoc file "
 git push origin master:master
